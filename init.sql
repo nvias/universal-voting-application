d71ad2e380cf1c4e0be8128370e9915c437ae339
Marek Ruttner <45697300+marekruttner@users.noreply.github.com>
2025-06-19 18:23:20 +0200

    Update init.sql

--- init.sql
+++ init.sql
@@ -2,10 +2,10 @@
 -- This script runs when the PostgreSQL container starts for the first time
 
 -- Create database if it doesn't exist (optional, as it's created by POSTGRES_DB)
-CREATE DATABASE IF NOT EXISTS voting_db;
+CREATE DATABASE voting_db;
 
 -- Create extension for UUID generation (optional)
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE EXTENSION "uuid-ossp";
 
 -- Grant privileges
 GRANT ALL PRIVILEGES ON DATABASE voting_db TO postgres;
